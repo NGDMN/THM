7b827df19b661ac515b51f4137c7c200c4307f9d
Neil Goodman <neilgoodman@live.com>
2025-05-17 19:56:15 -0300

    Refatorar projeto: remover arquivos desnecessários e simplificar estrutura

--- start.sh
+++ start.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# Atualizar pip e instalar dependências binarias
+# Definir flag para usar versões binárias do numpy e outras bibliotecas
+export PIP_ONLY_BINARY=numpy,scipy,pandas,scikit-learn
+
+# Atualizar pip e instalar dependências
 pip install --upgrade pip
-pip install -r requirements-deploy.txt
+
+# Entrar na pasta da API e instalar dependências
+cd api
+pip install -r requirements.txt
 
 # Iniciar a API
-cd api
 gunicorn app:app --bind 0.0.0.0:$PORT 
